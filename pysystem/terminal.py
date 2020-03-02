f5fad2d9ca86f42ab7737a6fc8c71f45ef66ec95
martinkozle <martinkozle@yahoo.com>
2020-03-02 19:00:15 +0100

    added prompt_toolkit input

--- pysystem/terminal.py
+++ pysystem/terminal.py
@@ -2,6 +2,12 @@ from pysystem import commands
 import pysystem.exceptions
 import traceback
 
+from prompt_toolkit import prompt
+from prompt_toolkit.history import FileHistory
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
+from prompt_toolkit.contrib.completers import SystemCompleter
+from prompt_toolkit.completion import WordCompleter
+
 
 class Terminal:
     def __init__(self, system):
@@ -11,6 +17,7 @@ class Terminal:
         self.terminal_commands = {
             'exit': None,
             'history': None,
+            'help': None,
             'cat': commands.cat,
             'touch': commands.touch,
             'mkdir': commands.mkdir,
@@ -25,10 +32,22 @@ class Terminal:
             'move': commands.mv,
             'tree': commands.tree
         }
+        self.completer = None
+
+    def update_completer(self):
+        self.completer = WordCompleter(
+            list(self.terminal_commands.keys()) + [child_file for child_file in self.current_folder.child_files.keys()],
+            ignore_case=True)
 
     def interpret(self):
         while True:
-            terminal_input = input(self.current_folder.get_path() + '> ')
+            # terminal_input = input(self.current_folder.get_path() + '> ')
+            self.update_completer()
+            terminal_input = prompt(self.current_folder.get_path() + '> ',
+                                    history=FileHistory('command_history.txt'),
+                                    auto_suggest=AutoSuggestFromHistory(),
+                                    completer=self.completer,
+                                    )
             for terminal_command in terminal_input.split('&'):
                 terminal_command = terminal_command.strip().split(' ')
                 self.input_history.append(terminal_input)
