b3be82406268472d681589f79dc1a78dac6fe5dd
martinkozle <martinkozle@yahoo.com>
2020-03-02 15:23:52 +0100

    added history command, shows terminal history, also fixed output for entering a wrong command

--- pysystem/terminal.py
+++ pysystem/terminal.py
@@ -7,8 +7,10 @@ class Terminal:
     def __init__(self, system):
         self.system = system
         self.current_folder = system.root_folder
+        self.input_history = []
         self.terminal_commands = {
             'exit': None,
+            'history': None,
             'cat': commands.cat,
             'touch': commands.touch,
             'mkdir': commands.mkdir,
@@ -21,14 +23,17 @@ class Terminal:
 
     def interpret(self):
         while True:
-            terminal_input = input(self.current_folder.get_path() + '> ').split('&')
-            for terminal_command in terminal_input:
+            terminal_input = input(self.current_folder.get_path() + '> ')
+            for terminal_command in terminal_input.split('&'):
                 terminal_command = terminal_command.strip().split(' ')
+                self.input_history.append(terminal_input)
                 if terminal_command[0] == 'exit':
                     print('Exiting...')
                     return
-                if terminal_command[0] == 'help':
+                elif terminal_command[0] == 'help':
                     print('Available commands:\n\t' + '\n\t'.join(self.terminal_commands.keys()))
+                elif terminal_command[0] == 'history':
+                    print('Input history:\n\t' + '\n\t'.join(self.input_history))
                 elif terminal_command[0] in self.terminal_commands:
                     try:
                         print(self.terminal_commands[terminal_command[0]](self.system, self, *terminal_command))
@@ -40,7 +45,7 @@ class Terminal:
                         print(traceback.format_exc())
                         break
                 else:
-                    print("'{}' is not recognized as an internal or external command.".format(terminal_input[0]))
+                    print("'{}' is not recognized as an internal or external command.".format(terminal_command[0]))
 
 
 def interpret(system):
